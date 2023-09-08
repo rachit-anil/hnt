005a37586fdd3e37ccc1e13133bca8adf6db04a3
rachit-anil <rachit9910102312@gmail.com>
2023-09-09 03:33:14 +0530

    initial react app creation

---
+++ Frontend/hnt/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>Here N there</div>
+    </>
+  );
+}
+
+export default App;
