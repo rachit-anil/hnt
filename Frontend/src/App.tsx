0340db3bf411230cddc181657c65424f57a14e4c
rachit-anil <rachit9910102312@gmail.com>
2023-09-10 23:23:39 +0530

    checking Front End file

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+function fetchCall() {
+  fetch('http://13.127.51.92:8080/').then(x => x.json()).then(x => console.log(x));
+}
 function App() {
   return (
     <>
-    <div>Here N there</div>
+      <button onClick={fetchCall}>Click</button>
+      <div>Here N there</div>
     </>
   );
 }
