8b79ba15e9813014e88db0aafdc701bfcf1cbc3c
rachit-anil <rachit9910102312@gmail.com>
2023-09-18 02:01:23 +0530

    adding support for scss

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 import config from './config/config';
-
+import Login from './pages/authentication/login';
 
 function fetchCall() {
   fetch(config.APP_URL).then(x => x.json()).then(x => console.log(x));
@@ -10,8 +10,12 @@ function fetchCall() {
 function App() {
   return (
     <>
+      <Login />
       <button onClick={fetchCall}>Click</button>
-      <div>Here N there</div>
+      <div className={styles.parent}>
+        Parent
+        <h1 className='child'>Child</h1>
+      </div>
       <div>{process.env.REACT_APP_API_URL}</div>
     </>
   );
---
+++ Frontend/src/pages/authentication/login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './login.module.scss';
+
+
+function Login() {
+    const a = 'authencationStyles'
+    return (
+        <>
+            <div className={styles.parent}>Parent</div>
+            <div className={false ? '' : styles[a]}> Login</div>
+            <div className='app-comp'> App comp style ??</div>
+            <div className='index-style'> Index style</div>
+        </>
+    );
+}
+
+export default Login;
